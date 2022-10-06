732e00fd2d02376b6433b1b9f135068caf402be5
”Ismail” <ibeno95@mail.ru>
2022-10-06 15:47:41 +0300

    убрал комнаты в чате, сделал общую автоматом, добавил проверку на авторизацию, в хедере кнопка чата появляется только после авторизации, имя юзера берется из локального хранилища

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,40 +7,59 @@ import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  const token = useSelector((state) => state.application.token);
 
-  const id = useSelector(state => state.application.id)
+  const id = useSelector((state) => state.application.id);
 
-  const [modal, setModal] = useState(false)
+  const [modal, setModal] = useState(false);
 
-const handleAuth = () => {
-  setModal(true)
-}
+  const handleAuth = () => {
+    setModal(true);
+  };
 
   return (
     <>
       {modal ? <Auth setModal={setModal} /> : null}
       <div className={styles.header}>
-      <div className={styles.header_items}>
-        <div className={styles.logo}>
-          <img src="logo.png" alt="" />
+        <div className={styles.header_items}>
+          <div className={styles.logo}>
+            <img src="logo.png" alt="" />
+          </div>
+          <p>
+            <Link to="/" className={styles.site_name}>
+              GogoLь-MogoLь
+            </Link>
+          </p>
+          <div>
+            <input className={styles.header_input} type="text" />
+            <img className={styles.lupa} src="lupa.png" alt="" />
+          </div>
         </div>
-        <p><Link to="/" className={styles.site_name} >GogoLь-MogoLь</Link></p>
-        <div>
-          <input className={styles.header_input} type="text" />
-          <img className={styles.lupa} src="lupa.png" alt="" />
+        <div className={styles.review_and_btn}>
+          {token && (
+            <p>
+              <Link to="/chat" className={styles.chat_link}>
+                Чат
+              </Link>
+            </p>
+          )}
+
+          <div className={styles.review}>
+            <img src="pencil.png" alt="" />
+            <p>Отзыв</p>
+          </div>
+          {id ? (
+            <Link to={`/personal/${id}`} className={styles.profil}>
+              <FontAwesomeIcon icon={faUserSecret} />
+            </Link>
+          ) : (
+            <button onClick={handleAuth} className={styles.auth_btn}>
+              Войти
+            </button>
+          )}
         </div>
       </div>
-      <div className={styles.review_and_btn}>
-        <p ><Link to="/chat" className={styles.chat_link} >Чат</Link></p>
-        <div className={styles.review}>
-          <img src="pencil.png" alt="" />
-          <p>Отзыв</p>
-        </div>
-        {id ? <Link to={`/personal/${id}`} className={styles.profil}><FontAwesomeIcon icon={faUserSecret} /></Link> : <button onClick={handleAuth} className={styles.auth_btn}>Войти</button>}
-      </div>
-    </div>
     </>
-  
   );
 };
 

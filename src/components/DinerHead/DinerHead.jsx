2ca7175078a38957a49026f4b3ceef7b846b5465
Djabril <dsadulaev@mail.ru>
2022-10-09 03:58:27 +0300

    настроил слайсы для ревью

--- src/components/DinerHead/DinerHead.jsx
+++ src/components/DinerHead/DinerHead.jsx
@@ -10,6 +10,7 @@ import { useParams } from "react-router-dom";
 
 const DinerHead = ({ diner }) => {
   const { dinerId } = useParams();
+  console.log(diner)
   const userId = useSelector((state) => state.application.id);
   const users = useSelector((state) => state.users.users);
 
@@ -43,7 +44,7 @@ const DinerHead = ({ diner }) => {
         />
         <FontAwesomeIcon className={styles.feather} icon={faFeather} />
       </div>
-      <div className={styles.diner_name}>{diner.map((elem) => <h1 key={elem._id}>{elem.name}</h1>)}</div>
+      <div className={styles.diner_name}><h1 key={diner._id}>{diner.name}</h1></div>
       <div className={styles.links}>
         <FontAwesomeIcon className={styles.instagram} icon={faInstagram} />
         <span>+7(938) 569-32-43</span>
--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,11 +1,78 @@
-import React from 'react';
+import React from "react";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
+import styles from "./review.module.css";
 
 const Review = () => {
-    return (
-        <div>
-            
+  const dispatch = useDispatch();
+  const { dinerId } = useParams();
+
+  const star = ["★", "★", "★", "★", "★"];
+  const [rating, setRating] = useState(0);
+  const [review, setReview] = useState("");
+  const [plus, setPlus] = useState("");
+  const [minus, setMinus] = useState("");
+
+  const handleRevievChange = (e) => {
+    setReview(e.target.value);
+  };
+  const handlePlusChange = (e) => {
+    setPlus(e.target.value);
+  };
+  const handleMinusChange = (e) => {
+    setMinus(e.target.value);
+  };
+
+  return (
+    <div>
+      <div className={styles.main}>
+        <div className="ratingDiv">
+          <h2>Ваша оценка</h2>
+          <div>
+            {star.map((item, index) => {
+              return (
+                <button
+                  className={
+                    index + 1 < rating ? styles.focus : styles.disfocus
+                  }
+                  onFocus={() => setRating(index + 2)}
+                >
+                  {item}
+                </button>
+              );
+            })}
+          </div>
         </div>
-    );
+        <div className="comment">
+          <input
+            value={review}
+            onChange={(e) => handleRevievChange(e)}
+            placeholder="начните писать отзыв"
+            type="text"
+          />
+        </div>
+        <div className="plusMinus">
+          <input
+            value={plus}
+            onChange={(e) => handlePlusChange(e)}
+            placeholder="плюсы ресторана"
+            type="text"
+          />
+        </div>
+        <div>
+          <input
+            value={minus}
+            onChange={(e) => handleMinusChange(e)}
+            placeholder="минусы ресторана"
+            type="text"
+          />
+        </div>
+        <button>опубликовать</button>
+      </div>
+    </div>
+  );
 };
 
 export default Review;

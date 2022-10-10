ce2a5a316ea1675237fdc912722afebb8faf44d6
Djabril <dsadulaev@mail.ru>
2022-10-10 07:20:41 +0300

    закончил работу с отзывами

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -3,41 +3,88 @@ import { useState } from "react";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { rateDiner } from "../../features/dinerSlice";
+import { addReview, fetchReviews } from "../../features/commentSlice";
 import styles from "./review.module.css";
 
 const Review = () => {
   const dispatch = useDispatch();
   const { dinerId } = useParams();
 
+  useEffect(() => {
+    dispatch(fetchReviews());
+  }, [dispatch]);
+
+  const reviews = useSelector((state) => state.comments.reviews);
+
+
   const star = ["★", "★", "★", "★", "★"];
   const [rating, setRating] = useState(0);
-  const [review, setReview] = useState("");
+  const [text, setText] = useState("");
   const [plus, setPlus] = useState("");
   const [minus, setMinus] = useState("");
+  const [disable, setDisable] = useState(true);
+  const [visible, setVisible] = useState(false)
 
   const handleRevievChange = (e) => {
-    setReview(e.target.value);
+    setText(e.target.value);
+    if (rating !== 0) {
+      if (text !== "") {
+        if (plus !== "") {
+          if (minus !== "") {
+            setDisable(false);
+          }
+        }
+      }
+    }
   };
   const handlePlusChange = (e) => {
     setPlus(e.target.value);
+    if (rating !== 0) {
+      if (text !== "") {
+        if (plus !== "") {
+          if (minus !== "") {
+            setDisable(false);
+          }
+        }
+      }
+    }
   };
   const handleMinusChange = (e) => {
     setMinus(e.target.value);
+    if (rating !== 0) {
+      if (text !== "") {
+        if (plus !== "") {
+          if (minus !== "") {
+            setDisable(false);
+          }
+        }
+      }
+    }
   };
+  const handleClick = () => {
+    dispatch(rateDiner({ rating, dinerId }));
+    dispatch(addReview({ text, plus, minus, dinerId, rating }));
+    window.location.reload()
+  };
+  const handleVisible = () => {
+    setVisible(true)
+  }
 
   return (
-    <div>
-      <div className={styles.main}>
+    <div className={styles.glav}>
+      {visible? <div className={styles.main}>
         <div className="ratingDiv">
-          <h2>Ваша оценка</h2>
-          <div>
+          <h3 className={styles.title}>поставьте оценку</h3>
+          <div className={styles.stars}>
             {star.map((item, index) => {
               return (
                 <button
                   className={
-                    index + 1 < rating ? styles.focus : styles.disfocus
+                    index + 1 <= rating ? styles.focus : styles.disfocus
                   }
-                  onFocus={() => setRating(index + 2)}
+                  onFocus={() => setRating(index + 1)}
                 >
                   {item}
                 </button>
@@ -45,32 +92,97 @@ const Review = () => {
             })}
           </div>
         </div>
-        <div className="comment">
-          <input
-            value={review}
+        <div className={styles.reviewDiv}>
+          <textarea
+            className={styles.reviewText}
+            value={text}
             onChange={(e) => handleRevievChange(e)}
             placeholder="начните писать отзыв"
             type="text"
           />
         </div>
-        <div className="plusMinus">
-          <input
-            value={plus}
-            onChange={(e) => handlePlusChange(e)}
-            placeholder="плюсы ресторана"
-            type="text"
+        <div className={styles.plusMinus}>
+          <img
+            className={styles.img}
+            src="http://localhost:4000/img/plusminus.png"
+            alt="plus"
           />
+          <div className={styles.midle}>
+            <div className={styles.plusMinusDiv}>
+              <input
+                className={styles.imputPlusMinus}
+                value={plus}
+                onChange={(e) => handlePlusChange(e)}
+                placeholder="плюсы ресторана"
+                type="text"
+              />
+            </div>
+            <div className={styles.plusMinusDiv}>
+              <input
+                className={styles.imputPlusMinus}
+                value={minus}
+                onChange={(e) => handleMinusChange(e)}
+                placeholder="минусы ресторана"
+                type="text"
+              />
+            </div>
+          </div>
         </div>
-        <div>
-          <input
-            value={minus}
-            onChange={(e) => handleMinusChange(e)}
-            placeholder="минусы ресторана"
-            type="text"
-          />
-        </div>
-        <button>опубликовать</button>
+        <button
+          disabled={disable}
+          className={styles.btn}
+          onClick={() => handleClick()}
+        >
+          опубликовать
+        </button>
+      </div> : null }
+      
+      <div className={styles.reverse}>
+        {reviews.map((item) => {
+          return (
+            <div className={styles.commentMain}>
+              <div className={styles.commentHeader}>
+                <div className={styles.logo}>
+                  <img
+                    className={styles.imgCom}
+                    src="http://localhost:4000/img/avatar.png"
+                    alt="plus"
+                  />
+                  <p className={styles.p}>{item.user.login}</p>
+                </div>
+                <div className={styles.starsCom}>
+                  {star.map((element, index) => {
+                    return (
+                      <button
+                        className={
+                          index < item.rating ? styles.focusCom : styles.disfocusCom
+                        }
+                        onFocus={() => setRating(index + 1)}
+                      >
+                        {element}
+                      </button>
+                    );
+                  })}
+                </div>
+              </div>
+              <div className={styles.midleCom}>
+                <img
+                  className={styles.imgPlusCom}
+                  src="http://localhost:4000/img/plusminus.png"
+                  alt="plus"
+                />
+                <div className={styles.plusMinusCom}>
+                  <div className={styles.plusMinusText}>{item.plus}</div>
+                  <div className={styles.plusMinusText}>{item.minus}</div>
+                </div>
+              </div>
+              <div className={styles.textCom}>{item.text}</div>
+            </div>
+          );
+        })}
+        <div className={styles.btnDiv}> <button className={visible ? styles.hide: styles.btn} onClick={handleVisible}>Добавить отзыв</button></div>
       </div>
+      
     </div>
   );
 };

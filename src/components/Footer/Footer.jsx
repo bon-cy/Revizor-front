53eb39dd554a67ea013e368fa71219506cf8e3eb
boncy <boncy@mail.ru>
2022-10-11 12:06:03 +0300

    asdf

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -17,15 +17,7 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.subGeneral}>
-        <div className={styles.block1}>
-          <img
-            width="70px"
-            src={"http://localhost:4000/public/logo.png"}
-            alt=""
-          />
-
-        
-        </div>
+       
         <div className={styles.block2}>
         <p className={styles.content}>Работаем для Вас с 1991 года!</p>
           <FontAwesomeIcon fontSize="50px" className={styles.icon} icon={faInstagram} />

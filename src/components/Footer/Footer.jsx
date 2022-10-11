db273847b49b776d53171cb380852fe228f707bb
boncy <boncy@mail.ru>
2022-10-11 12:02:47 +0300

    adfasdf

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,33 +1,43 @@
 import React from "react";
 import styles from "./footer.module.css";
-import facebook from "../../public/facebook.png";
-import insta from "../../public/insta.png";
-import pochta from "../../public/pochta.png";
-import telega from "../../public/telega.png";
-import whatsapp from "../../public/whatsapp.png";
-import youtube from "../../public/youtube.png";
-import trip from "../../public/logo2.png";
+
+
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import {
+  faInstagram,
+  faFacebook,
+  faWhatsapp,
+  faTelegram,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.subGeneral}>
         <div className={styles.block1}>
-        <img width="70px" src={"http://localhost:4000/public/logo.png"} alt="" />
+          <img
+            width="70px"
+            src={"http://localhost:4000/public/logo.png"}
+            alt=""
+          />
 
-          <p>Работаем для Вас с 1991 года!</p>
+        
         </div>
         <div className={styles.block2}>
-          <img width="50px" src={facebook} />
-          <img width="50px" src={insta} />
-          <img width="50px" src={pochta} />
-          <img width="50px" src={telega} />
-          <img width="50px" src={whatsapp} />
-          <img width="50px" src={youtube} />
+        <p className={styles.content}>Работаем для Вас с 1991 года!</p>
+          <FontAwesomeIcon fontSize="50px" className={styles.icon} icon={faInstagram} />
+          <FontAwesomeIcon fontSize="50px" className={styles.icon} icon={faFacebook} />
+          <FontAwesomeIcon fontSize="50px" className={styles.icon} icon={faTelegram} />
+          <FontAwesomeIcon fontSize="50px" className={styles.icon} icon={faYoutube} />
+          <FontAwesomeIcon fontSize="50px" className={styles.icon} icon={faWhatsapp} />
+          <p className={styles.content}>8(800)-555-35-35</p>
         </div>
       </div>
       <div className={styles.dno}>
-        <p>© GogoLь-MogoLь, 2022 г. Все права защищены.</p>
+        © GogoLь-MogoLь, 2022 г. Все права защищены.
       </div>
     </footer>
   );
